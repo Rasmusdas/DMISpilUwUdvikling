6ab251d8ca77421812dbd14384302ca58cf10d3c
rasmusdas <rasmusmkn@gmail.com>
2019-10-04 19:17:16 +0200

    Added jumping and stuff uwu

---
+++ Assets/Scripts/MakeBridge.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBridge : MonoBehaviour
+{
+    public Vector3 spawnPoint;
+    public GameObject bridge;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        bridge.SetActive(true);
+        collision.transform.position = spawnPoint;
+        Camera.main.backgroundColor = Color.black;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float jumpSpeed;
+    Rigidbody2D rb;
+    bool jump = false;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Vector3 move = transform.right * (x * Time.deltaTime)*movementSpeed;
+        transform.Translate(move.x,move.y,move.z);
+        print(Physics2D.Raycast(transform.position, -transform.up, 0.5f,1));
+        if (Input.GetKeyDown(KeyCode.Space) && !jump)
+        {
+            rb.AddForce(transform.up*jumpSpeed);
+            jump = true;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            jump = false;
+        }
+    }
+}
