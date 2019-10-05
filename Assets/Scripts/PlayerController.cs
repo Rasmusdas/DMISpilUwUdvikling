97f652ab38976a5f0905e9d56deca8b006bd81fe
Tadayo <daniel.reffeldt@outlook.com>
2019-10-05 09:20:22 +0200

    Added jumpleciancy to playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed;
+    public float jumpLeniancy = 0.1f;
     public float jumpSpeed;
     public bool control = true;
     Rigidbody2D rb;
@@ -20,7 +22,7 @@ public class PlayerController : MonoBehaviour
             float x = Input.GetAxis("Horizontal");
             Vector3 move = transform.right * (x * Time.deltaTime * movementSpeed);
             rb.velocity = new Vector3(move.x,rb.velocity.y);
-            if (Input.GetKeyDown(KeyCode.Space) && Mathf.Approximately(rb.velocity.y,0))
+            if (Input.GetKeyDown(KeyCode.Space) && Math.Sqrt(rb.velocity.y*rb.velocity.y) < jumpLeniancy )
             {
                 rb.AddForce(transform.up * jumpSpeed,ForceMode2D.Impulse);
             }
