33cf061aa1d71b627b0ab3e951739a5ec0a79a2a
rasmusdas <rasmusmkn@gmail.com>
2019-10-05 06:28:28 +0200

    Renamed MakeBridge.cs to Spike.cs and added ChangeCamera.cs

---
+++ Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public GameObject oldCam;
+    public GameObject newCam;
+    private void OnTriggerEnter(Collider other)
+    {
+        newCam.SetActive(true);
+        oldCam.SetActive(false);
+    }
+}
--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -2,16 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MakeBridge : MonoBehaviour
+public class Spike : MonoBehaviour
 {
     public Vector3 spawnPoint;
-    public GameObject bridge;
     public GameObject player;
-    public Material owo;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        bridge.SetActive(true);
         Instantiate(player, spawnPoint,Quaternion.identity);
         collision.gameObject.GetComponent<PlayerController>().control = false;
         collision.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
