b63237ac8b3b5cd09eb127a9343069fcedd9b326
rasmusdas <rasmusmkn@gmail.com>
2019-10-05 22:16:45 +0200

    Added tilemap disabler

---
+++ Assets/Scripts/TilemapDisabler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilemapDisabler : MonoBehaviour
+{
+
+    public GameObject tileToDisable;
+    public GameObject tileToEnable;
+    public int requiredDeaths;
+    void Update()
+    {
+        if(requiredDeaths >= GameManager.timesDied && !tileToEnable.activeSelf)
+        {
+            tileToDisable.SetActive(false);
+            tileToEnable.SetActive(true);
+        }
+    }
+}
