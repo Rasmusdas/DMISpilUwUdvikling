ec8e8047a0d9a0ff7eef22e05bac64cf0febe13c
rasmusdas <rasmusmkn@gmail.com>
2019-10-05 07:26:04 +0200

    Added DisappearingPlatform.cs

---
+++ Assets/Scripts/DisappearingPlatform.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingPlatform : MonoBehaviour
+{
+    bool breaking;
+    public float breakTime;
+    public float reappearTime;
+    Color c;
+    SpriteRenderer sR;
+    private void Start()
+    {
+        c = GetComponent<SpriteRenderer>().color;
+        sR = GetComponent<SpriteRenderer>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Player" && !breaking )
+        {
+            breaking = true;
+            StartCoroutine(BreakPlatform());
+        }
+    }
+
+    IEnumerator BreakPlatform()
+    {
+        
+        c = new Color(c.r,c.g,c.b,c.a-(c.a/breakTime));
+        sR.color = c;
+        yield return new WaitForSeconds(breakTime / 100);
+        Debug.Log(c.a);
+        if (c.a > 0.01)
+        {
+            StartCoroutine(BreakPlatform());
+        }
+        else
+        {
+            StartCoroutine(RebuildPlatform());
+        }
+    }
+    IEnumerator RebuildPlatform()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = true;
+        yield return new WaitForSeconds(reappearTime);
+        GetComponent<BoxCollider2D>().isTrigger = false;
+        sR.color = new Color(c.r, c.g, c.b, 1);
+        c = new Color(c.r, c.g, c.b, 1);
+        breaking = false;
+    }
+}
