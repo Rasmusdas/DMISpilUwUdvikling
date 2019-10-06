d7820582c89875c0487f78f90426f7c69606d3bb
rasmusdas <rasmusmkn@gmail.com>
2019-10-06 12:53:01 +0200

    Renamed Hell.cs to EndGameScript.cs to better reflect the functionality (also idk why its endgamescript instead of just endgame :3 )

--- Assets/Scripts/EndGameScript.cs
+++ Assets/Scripts/EndGameScript.cs
@@ -2,26 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hell : MonoBehaviour
+public class EndGameScript : MonoBehaviour
 {
     public float fadeTime;
     public SpriteRenderer sR;
     public Color c;
-    private void Start()
-    {
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
-        {
-            StartCoroutine(GameObject.FindGameObjectWithTag("EndGame").GetComponent<Hell>().FadeToBlack());
+        if (other.tag == "Player")
+        { 
+            GameObject.FindGameObjectWithTag("EndGame").GetComponent<EndGameScript>().c = c;
+            StartCoroutine(GameObject.FindGameObjectWithTag("EndGame").GetComponent<EndGameScript>().FadeToBlack());
         }
     }
 
 
     IEnumerator FadeToBlack()
     {
-        c = sR.material.color;
         Debug.Log(c);
         yield return new WaitForSeconds(fadeTime/100);
         sR.material.color = new Color(c.r, c.g, c.b, c.a+0.01f);
